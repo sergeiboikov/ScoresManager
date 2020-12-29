fd24f94f6cb0e41bc6ad184ddf5d6225416a7d1f
Sergei Boikov <Sergei_Boikov@epam.com>
2020-12-29 11:44:55 +0400

    Added UserTypeId column to User table

--- ScoresManager.DB/dbo/Tables/User.sql
+++ ScoresManager.DB/dbo/Tables/User.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[User] (
     [UserId]       INT            IDENTITY (1, 1) NOT NULL,
-    [UserTypeId]   SMALLINT       NOT NULL,
     [Name]         NVARCHAR (100) NOT NULL,
     [Email]        NVARCHAR (100) NOT NULL,
     [Notes]        NVARCHAR (500) NULL,
@@ -8,7 +7,6 @@
     [sysChangedAt] DATETIME       DEFAULT (getutcdate()) NULL,
     [sysCreatedBy] INT            DEFAULT ((-1)) NULL,
     [sysChangedBy] INT            DEFAULT ((-1)) NULL,
-    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC),
-    CONSTRAINT [FK_User_UserType_UserTypeId] FOREIGN KEY ([UserTypeId]) REFERENCES [dbo].[UserType] ([UserTypeId])
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC)
 );
 
--- ScoresManager.DB/dbo/Views/vw_User.sql
+++ ScoresManager.DB/dbo/Views/vw_User.sql
@@ -1,8 +1,6 @@
 ﻿CREATE VIEW [dbo].[vw_User]
 AS
-SELECT   ut.[Name]      AS [UserType]
-        , u.[Name]      AS [UserName]
+SELECT    u.[Name]      AS [UserName]
         , u.[Email]     AS [UserEmail]  
         , u.[Notes]     AS [UserNotes]  
 FROM [dbo].[User] u
-INNER JOIN [dbo].[UserType] ut ON ut.UserTypeId = u.UserTypeId
