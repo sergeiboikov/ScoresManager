cee2c5afa9cd4a25bcd20d57eca0559a62ac7b9c
Sergei Boikov <Sergei_Boikov@epam.com>
2020-12-29 13:06:21 +0400

    Added default constraints to ScoresTemplates

--- ScoresManager.DB/mentor/Tables/ScoresTemplate.sql
+++ ScoresManager.DB/mentor/Tables/ScoresTemplate.sql
@@ -4,10 +4,10 @@
     [Accuracy]         INT      NOT NULL,
     [OnTime]           INT      NOT NULL,
     [Extra]            INT      NOT NULL,
-    [sysCreatedAt]     DATETIME DEFAULT (getutcdate()) NULL,
-    [sysChangedAt]     DATETIME DEFAULT (getutcdate()) NULL,
-    [sysCreatedBy]     INT      DEFAULT ((-1)) NULL,
-    [sysChangedBy]     INT      DEFAULT ((-1)) NULL,
+    [sysCreatedAt] DATETIME       CONSTRAINT [DF_ScoresTemplate_sysCreatedAt] DEFAULT (getutcdate()) NULL,
+    [sysChangedAt] DATETIME       CONSTRAINT [DF_ScoresTemplate_sysChangedAt] DEFAULT (getutcdate()) NULL,
+    [sysCreatedBy] INT            CONSTRAINT [DF_ScoresTemplate_sysCreatedBy] DEFAULT ((-1)) NULL,
+    [sysChangedBy] INT            CONSTRAINT [DF_ScoresTemplate_sysChangedBy] DEFAULT ((-1)) NULL,
     CONSTRAINT [PK_mentor.ScoresTemplate] PRIMARY KEY CLUSTERED ([ScoresTemplateId] ASC)
 );
 
