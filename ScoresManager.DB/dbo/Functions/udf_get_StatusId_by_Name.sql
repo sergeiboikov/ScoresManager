1ea26b115c543a8f16c046a8ab4deb2d86f7a050
Sergei Boikov <Sergei_Boikov@epam.com>
2021-09-09 09:58:28 +0400

    Added metadata for StudentStatus

---
+++ ScoresManager.DB/dbo/Functions/udf_get_StatusId_by_Name.sql
@@ -0,0 +1,13 @@
+﻿CREATE FUNCTION dbo.udf_get_StatusId_by_Name(@StatusName NVARCHAR(100))  
+RETURNS SMALLINT   
+AS   
+-- Returns StatusId for Name
+BEGIN  
+    DECLARE @ret int;  
+
+    SELECT @ret = s.StatusId
+    FROM [dbo].[Status] s
+    WHERE s.[Name] = @StatusName;
+
+    RETURN @ret;  
+END;
