ab520e9db24448e6280ad0a0610bc872b80ae2ad
Elyana Pogosyan <Elyana_Pogosyan@epam.com>
2021-10-26 18:25:04 +0400

    Added SPs [dbo].[usp_CheckScriptType_Insert] and [dbo].[usp_ConnectionType_Insert]. Changed view vw_CheckScript

---
+++ ScoresManager.DB/dbo/Stored Procedures/usp_CheckScriptType_Insert.sql
@@ -0,0 +1,47 @@
+﻿-- =============================================
+-- Author:			Elyana Pogosyan
+-- Create Date:		2021-10-26
+-- Description: Load information for CheckScriptType from JSON
+-- Format JSON: N'["CheckScriptTypeName":"SQL"]' 
+--  Example: EXEC [dbo].[usp_CheckScriptType_Insert] @json = N'["CheckScriptTypeName":"SQL"]' 
+
+-- =============================================
+
+
+CREATE PROCEDURE [dbo].[usp_CheckScriptType_Insert]
+(
+    @json NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+IF ISJSON(@json) > 0
+	BEGIN
+        BEGIN TRY
+	        BEGIN TRANSACTION
+                DROP TABLE IF EXISTS #TEMP_SOURCE;
+
+			    SELECT *
+			    INTO #TEMP_SOURCE
+				FROM OPENJSON(@json)
+                WITH (
+                         CheckScriptTypeName NVARCHAR(250)	 '$.CheckScriptTypeName'
+                ) AS RootL;
+
+				-- CheckScriptType insert
+
+				INSERT INTO [dbo].[CheckScriptType] (Name)
+				SELECT tmp.[CheckScriptTypeName]
+				FROM #TEMP_SOURCE tmp;
+
+				DROP TABLE #TEMP_SOURCE;
+            COMMIT TRANSACTION
+        END TRY
+        BEGIN CATCH
+	            IF @@TRANCOUNT > 0 
+		            ROLLBACK TRANSACTION;
+	            THROW;
+        END CATCH;
+    END
+END
--- ScoresManager.DB/dbo/Views/vw_CheckScript.sql
+++ ScoresManager.DB/dbo/Views/vw_CheckScript.sql
@@ -8,8 +8,8 @@ SELECT  ST.[SubTaskId]
 	,	CST.[Name]			AS [CheckScriptTypeName]
 	,	C.[ConnectionString]
 	,	CT.[Name]			AS [ConnectionTypeName]
-FROM		   [dbo].[SubTask]			ST	
-	INNER JOIN [dbo].[CheckScript]		CS	ON CS.[CheckScriptId]	   = ST.[CheckScriptId]
-	INNER JOIN [dbo].[CheckScriptType]	CST ON CST.[CheckScriptTypeId] = CS.[CheckScriptTypeId]
-	INNER JOIN [dbo].[Connection]		C	ON C.[ConnectionId]		   = CS.[ConnectionId]
-	INNER JOIN [dbo].[ConnectionType]	CT	ON CT.[ConnectionTypeId]   = C.[ConnectionTypeId]
+FROM		  [dbo].[SubTask]			ST	
+	LEFT JOIN [dbo].[CheckScript]		CS	ON  CS.[CheckScriptId]	   = ST.[CheckScriptId]
+	LEFT JOIN [dbo].[CheckScriptType]	CST ON CST.[CheckScriptTypeId] = CS.[CheckScriptTypeId]
+	LEFT JOIN [dbo].[Connection]		C	ON   C.[ConnectionId]	   = CS.[ConnectionId]
+	LEFT JOIN [dbo].[ConnectionType]	CT	ON  CT.[ConnectionTypeId]  =  C.[ConnectionTypeId]
