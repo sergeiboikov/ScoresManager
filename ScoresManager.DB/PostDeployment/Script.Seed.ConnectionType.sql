be27ef3ac091380e473c394fe0590ff7eb29fedc
Sergei Boikov <Sergei_Boikov@epam.com>
2021-11-06 16:36:04 +0400

    Modifed the Script.Seed.ConnectionType script. ConnectionName and ConnectionTypeDescription renamed to ConnectionTypeName and ConnectionTypeDescription

--- ScoresManager.DB/PostDeployment/Script.Seed.ConnectionType.sql
+++ ScoresManager.DB/PostDeployment/Script.Seed.ConnectionType.sql
@@ -1,5 +1,5 @@
 ﻿DECLARE @JSON_CONFIG  NVARCHAR(MAX);
-SELECT @JSON_CONFIG = N'[{"ConnectionName":"ODBC", "ConnectionDescription":"Application setting name in Function App Configuration (ODBC connection string)"}]'
+SELECT @JSON_CONFIG = N'[{"ConnectionTypeName":"ODBC", "ConnectionTypeDescription":"Application setting name in Function App Configuration (ODBC connection string)"}]'
 
 EXEC [dbo].[usp_ConnectionType_Insert] @JSON_CONFIG
 GO
